0b093184bdfa19691a66cf9a183e37e235b1835f
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-16 00:54:22 +0600

    router

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route path='/shop' element={<Shop/>}/>
         <Route path='/orders' element={<OrderReview/>}/>
