4bf8c7ef4983e7ded32cb1f24627f20fef66d4d6
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-16 00:52:24 +0600

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/orders' element={<OrderReview/>}/>
+      </Routes>
       
       
-      <Header></Header>
-      <Shop></Shop>
     </div>
   );
 }
