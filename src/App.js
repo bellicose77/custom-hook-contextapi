a4955e02d49afcadb47857fc526f3460faae5212
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-18 23:16:25 +0600

    change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
 import OrderReview from './components/OrderReview/OrderReview';
 import { Routes, Route, Link } from "react-router-dom";
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/shop' element={<Shop/>}/>
         <Route path='/orders' element={<OrderReview/>}/>
+        <Route path='/inventory' element={<Inventory/>}/>
       </Routes>
       
       
