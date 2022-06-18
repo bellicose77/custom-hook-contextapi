224a05d0caf2f2d8b8e89850ac46748f965bdf36
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-18 23:16:50 +0600

    change

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-          <h2>This is nothing</h2>  
+          <h2>This is nothing</h2> 
+          <p>New things are coming</p> 
         </div>
     );
 };
